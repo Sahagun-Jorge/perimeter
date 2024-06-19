e27e88d6946a2acb1aec740284b0889846059d49
Jorge Sahagun <jrssa6@gmail.com>
2024-06-19 10:06:57 -0600

    remove loading permanent render

--- src/components/navigation/loading/Loading.tsx
+++ src/components/navigation/loading/Loading.tsx
@@ -1,5 +1,6 @@
 import { Box, CircularProgress } from "@mui/material";
 import React from "react";
+import { When } from "react-if";
 
 interface LoadingProps {
   isLoading: boolean;
@@ -7,22 +8,23 @@ interface LoadingProps {
 
 export const Loading = ({ isLoading }: LoadingProps) => {
   return (
-    <Box
-      sx={{
-        position: "fixed",
-        top: 0,
-        left: 0,
-        width: "100%",
-        height: "100%",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "rgba(0, 0, 0, 0.5)",
-        zIndex: 10000,
-        visibility: isLoading ? "visible" : "hidden",
-      }}
-    >
-      <CircularProgress />
-    </Box>
+    <When condition={isLoading}>
+      <Box
+        sx={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "rgba(0, 0, 0, 0.5)",
+          zIndex: 10000,
+        }}
+      >
+        <CircularProgress />
+      </Box>
+    </When>
   );
 };
